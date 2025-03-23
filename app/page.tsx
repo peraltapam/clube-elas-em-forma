cf049392ba68b03ec6d49306c2737defab8295ec
Pamela Peralta <pamperalta@gmail.com>
2025-03-23 09:45:26 -0300

    update under construction page message

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Clube Elas em Forma</h1>;
+  return (
+    <>
+      <h1>Clube Elas em Forma</h1>
+      <p>... em construção! ...</p>
+    </>
+  );
 }
